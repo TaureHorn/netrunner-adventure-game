5565a93f0ff9f3fdd325762df4437b3bb7c4d4a8
TaureHorn <abaird0893@gmail.com>
2023-01-28 00:31:25 +0000

    v1.0 of HTML and CSS, without functioning Javascript

---
+++ netrunner_js.js
@@ -0,0 +1,15 @@
+function toggleHelp() {
+    if (document.getElementById("help-section").style = "display:none") {
+        document.getElementById("help-section").style = "display:block;";
+    } else if (document.getElementById("help-section").style = "display:block;") {
+        document.getElementById("help-section").style = "display:none";
+    }
+}
+
+function toggleMisc()) {
+    if (document.getElementById("misc-section").style = "display:none") {
+        document.getElementById("misc-section").style = "display:block;";
+    } else if (document.getElementById("misc-section").style = "display:block;") {
+        document.getElementById("misc-section").style = "display:none";
+    }
+}

78145e5583eae21f2fb09e0b6416272f86a36172
TaureHorn <abaird0893@gmail.com>
2023-01-29 02:00:03 +0000

    Added 90% of HTML, CSS functionality. Toggleable headers with content and some simple JS to control it all.

--- netrunner_js.js
+++ netrunner_js.js
@@ -1,15 +1,156 @@
+
+//////////////////////////////////////// BUTTON TOGGLERS ////////////////////////
+
 function toggleHelp() {
-    if (document.getElementById("help-section").style = "display:none") {
-        document.getElementById("help-section").style = "display:block;";
-    } else if (document.getElementById("help-section").style = "display:block;") {
-        document.getElementById("help-section").style = "display:none";
+    let x = document.getElementById("help-section");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
     }
 }
 
-function toggleMisc()) {
-    if (document.getElementById("misc-section").style = "display:none") {
-        document.getElementById("misc-section").style = "display:block;";
-    } else if (document.getElementById("misc-section").style = "display:block;") {
-        document.getElementById("misc-section").style = "display:none";
+function toggleReadMeHelp() {
+    let x = document.getElementById("readme");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function toggleShellHelp() {
+    let x = document.getElementById("shell-help");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function toggleGlossary() {
+    let x = document.getElementById("glossary");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function toggleMisc() {
+    let x = document.getElementById("misc-section");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function toggleReadMeMisc() {
+    let x = document.getElementById("readme-misc");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function toggleMusic() {
+    let x = document.getElementById("music");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function toggleThemes() {
+    let x = document.getElementById("themes");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+//////////////////////////////////////// SUBHEADER TOGGLERS ////////////////////////
+
+function toggleLs() {
+    let x = document.getElementById("ls");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function toggleCat() {
+    let x = document.getElementById("cat");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function toggleClear() {
+    let x = document.getElementById("clear");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function toggleFile() {
+    let x = document.getElementById("file");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function toggleSsh() {
+    let x = document.getElementById("ssh");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function toggleSteghide() {
+    let x = document.getElementById("steghide");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function toggleGlossaryArasaka() {
+    let x = document.getElementById("arasaka");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function toggleGlossaryMilitech() {
+    let x = document.getElementById("militech");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function toggleGlossaryNetwatch() {
+    let x = document.getElementById("netwatch");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
     }
 }
